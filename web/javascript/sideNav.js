fac0aa40b7bde15e2a5a802e3e4e4afb37143b60
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-07-28 19:00:24 +0200

    Modification de la partie A propos

--- web/javascript/sideNav.js
+++ web/javascript/sideNav.js
@@ -1,4 +1,10 @@
 // Initialize collapse button
 $(".button-collapse").sideNav({
-    menuWidth: 250
+    menuWidth: 250,
+    onOpen: function(el) {
+        $('.hamburger').text('arrow_back');
+    }, // A function to be called when sideNav is opened
+      onClose: function(el) {
+          $('.hamburger').text('menu');
+      }
 });
